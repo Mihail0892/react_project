227d466fb10327c2cc8079abd3ce4df6ff415b8c
Mihail0892 <yanamishatereshchenko@gmail.com>
2023-03-01 11:54:18 +0200

    1280 and scroll

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import stylesSearchPage from "./SearchPage.module.scss";
 import Search from "components/Search/Search";
 
 const SearchPage = ({ setCart }) => {
+  const fieldRef = useRef(null);
+  useEffect(()=>{
+    fieldRef.current.scrollIntoView({behavior: 'smooth', block: 'start'});
+  },[])
   return (
-    <div className={stylesSearchPage.all_box}>
+    <div ref={fieldRef} className={stylesSearchPage.all_box}>
       <div className={stylesSearchPage.flex}>
         <p className={stylesSearchPage.left_text}>Пошук товару</p>
       </div>
